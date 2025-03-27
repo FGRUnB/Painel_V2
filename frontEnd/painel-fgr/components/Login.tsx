0d03d72a73b7033e7d780e2762caccd23e5660b5
joaolobo10 <joaocarloslobo10@gmail.com>
2025-03-27 13:25:12 -0300

    fix: ajuste de responsividade

--- frontEnd/painel-fgr/components/Login.tsx
+++ frontEnd/painel-fgr/components/Login.tsx
@@ -9,7 +9,7 @@ export function Login() {
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="text-5xl font-bold text-zinc-50 mb-8">Login</h1>
+      <h1 className="text-3xl mb-4 md:text-5xl font-bold text-zinc-50 md:mb-8">Login</h1>
       <div className="mb-4">
         <div className="flex items-center bg-zinc-900 p-2 rounded">
           <span className="text-zinc-100 mr-2 text-[1.4rem]">
@@ -17,7 +17,7 @@ export function Login() {
           </span>
           <input
             type="text"
-            className="bg-transparent w-75 h-10 focus:outline-none text-zinc-200"
+            className="bg-transparent w-50 md:w-75 h-10 focus:outline-none text-zinc-200"
             placeholder="Matrícula"
           />
         </div>
@@ -29,20 +29,20 @@ export function Login() {
           </span>
           <input
             type={showPassword ? "text" : "password"}
-            className="bg-transparent w-66 h-10 focus:outline-none text-zinc-200"
+            className="bg-transparent w-41 md:w-66 h-10 focus:outline-none text-zinc-200"
             placeholder="Senha"
           />
           <button
             type="button"
             onClick={() => setShowPassword(!showPassword)}
-            className="text-zinc-400 hover:text-zinc-100 transition-colors p-2"
+            className="text-zinc-400 hover:text-zinc-100 transition-colors p-2 cursor-pointer"
           >
             {showPassword ? <FiEyeOff size={20} /> : <FiEye size={20} />}
           </button>
         </div>
         <a
           href="#"
-          className="text-green-400 text-[1rem] mt-2 inline-block float-right  hover:text-green-400/40 transition-colors duration-200"
+          className="text-green-400 text-sm md:text-[1rem] mt-2 inline-block float-right  hover:text-green-400/40 transition-colors duration-200"
         >
           Esqueceu a senha?
         </a>
@@ -50,9 +50,9 @@ export function Login() {
       <button
         className="w-full bg-green-400 text-zinc-800 py-3 
       rounded-xl font-bold overflow-hidden hover:bg-green-400/30 
-      transition-colors cursor-pointer"
+      transition-colors duration-300 cursor-pointer"
       >
-        <span className="text-[1.2rem]">Entrar</span>
+        <span className="md:text-[1.2rem]">Entrar</span>
       </button>
     </div>
   );
